07d0114d3e90d7031be46ed7ff693fda7c37d4cd
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-12-16 23:42:35 +0600

    pass data controller to view

--- FirstProject/Controllers/HomeController.cs
+++ FirstProject/Controllers/HomeController.cs
@@ -6,27 +6,12 @@ namespace FirstProject.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public ViewResult Index()
     {
-        _logger = logger;
-    }
+        int hour = DateTime.Now.Hour;
+        string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View("MyView", viewModel);
     }
 }
 
